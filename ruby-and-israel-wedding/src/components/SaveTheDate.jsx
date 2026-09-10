a73d3289012ba7505fcccd6762fbb248b1fcfcc8
Jburgei <joyburgei4@gmail.com>
2026-09-10 22:09:41 +0300

    update savethedate

--- ruby-and-israel-wedding/src/components/SaveTheDate.jsx
+++ ruby-and-israel-wedding/src/components/SaveTheDate.jsx
@@ -7,7 +7,7 @@ export default function SaveTheDate() {
     <section className="std">
       <Reveal>
         <p className="eyebrow">Save the date</p>
-        <div className="date-weekday">Saturday</div>
+        <div className="date-weekday">Friday</div>
         <div className="date-words">The Fourth of December</div>
         <div className="date-year">2026</div>
         <div className="std-divider" />
